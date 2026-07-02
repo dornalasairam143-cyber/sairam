69a4ae968c16fd64e786b2fccae062527ec32f42
dornalasairam143-cyber <dornalasairam143@gmail.com>
2026-07-02 10:39:27 +0530

    Create build.bat

---
+++ build.bat
@@ -0,0 +1,28 @@
+@echo off
+title Building Dashboard Launcher
+
+echo.
+echo ===============================
+echo Building Dashboard Launcher...
+echo ===============================
+echo.
+
+if exist build rmdir /s /q build
+if exist dist rmdir /s /q dist
+if exist "Dashboard Launcher.spec" del /f /q "Dashboard Launcher.spec"
+
+pyinstaller ^
+--onefile ^
+--windowed ^
+--clean ^
+--icon=launcher.ico ^
+--name="Dashboard Launcher" ^
+launcher.py
+
+echo.
+echo ===============================
+echo BUILD COMPLETED
+echo ===============================
+echo.
+
+pause
